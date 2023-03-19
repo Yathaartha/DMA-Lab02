4f370432ab553e4c6475a26fcc442acccaacee8d
Yathaartha Maharjan <maharjanyathaartha017@gmail.com>
2023-03-19 20:49:36 +0545

    add comments

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -10,6 +10,9 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    /*
+    initialize the variables
+     */
     private lateinit var convertBtn: Button
     private lateinit var tipOptions: RadioGroup
     @SuppressLint("UseSwitchCompatOrMaterialCode")
@@ -21,17 +24,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // get the views
         convertBtn = findViewById(R.id.convert_button)
         tipOptions = findViewById(R.id.tip_options)
         roundSwitch = findViewById(R.id.round_up_switch)
         conversionResult = findViewById(R.id.converted_weight)
         weightToConvert = findViewById(R.id.weight_to_convert)
 
+        // add clickListener for convert button
         convertBtn.setOnClickListener {
             val text = weightToConvert.text.toString()
             var conversionRate = 0.0
             var result = 0.0
 
+            // check which radio button is selected and assign value
             conversionRate = when (tipOptions.checkedRadioButtonId) {
                 R.id.option_gm_to_cups -> 0.004
                 R.id.option_cups_to_gm -> 250.0
@@ -40,10 +46,15 @@ class MainActivity : AppCompatActivity() {
                 else -> 0.004
             }
 
+            // get the conversion result
             result = text.toFloat() * conversionRate
+
+            //check if rounding is needed
             if (roundSwitch.isChecked) {
                 result = result.toInt().toDouble()
             }
+
+            // display the result
             conversionResult.text = "Result: ${result.toString()}"
         }
     }
